541dc2322aa86337d6a53accef66121a5ba688d7
CelianKekenbosch <cekeken@gmail.com>
2021-03-31 15:38:16 +0200

    Ajout d'une ebauche de l'IA et de la classe joueur

---
+++ Bataille_Navale_Git/Bataille_Navale/src/main/java/Modele/Joueur.java
@@ -0,0 +1,12 @@
+package Modele;
+
+public class Joueur 
+{
+    private String name;
+    
+    //Constructeur de Joueur qui va juste assigner un pseudo au joueur
+    public Joueur(String name)
+    {
+        this.name = name;
+    }
+}
