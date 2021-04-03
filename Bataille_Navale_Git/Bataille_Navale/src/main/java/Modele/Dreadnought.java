e535c8909589b7e82b7786a4f2f8cd168596e6cc
CelianKekenbosch <cekeken@gmail.com>
2021-04-03 11:57:57 +0200

    Modif des supers des heritieres de ship

--- Bataille_Navale_Git/Bataille_Navale/src/main/java/Modele/Dreadnought.java
+++ Bataille_Navale_Git/Bataille_Navale/src/main/java/Modele/Dreadnought.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modele;
 
-public class Dreadnought extends Ship{
-    public Dreadnought(int size, int firePower, String typeShip) {// on instancie la longueur et la puissance du bateau en question
-        super(size, firePower, typeShip);
-        this.mSize=7;
-        this.mfirePower=9;
+public class Dreadnought extends Ship
+{
+    public Dreadnought(int size, int firePower, String typeShip) 
+    {// on instancie la longueur et la puissance du bateau en question
+        super(typeShip);
+        mSize=7;
+        mfirePower=9;
         this.mTypeShip="dreadnought";
     }
-    
 }
--- Bataille_Navale_Git/Bataille_Navale/src/main/java/Modele/Submarine.java
+++ Bataille_Navale_Git/Bataille_Navale/src/main/java/Modele/Submarine.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modele;
 
-public class Submarine extends Ship{
-    public Submarine(int size, int firePower, String typeShip) {// on instancie la longueur et la puissance du bateau en question
-        super(size, firePower, typeShip);
-        this.mSize=1;
-        this.mfirePower=1;
+public class Submarine extends Ship
+{
+    public Submarine(int size, int firePower, String typeShip) 
+    {// on instancie la longueur et la puissance du bateau en question
+        super(typeShip);
+        mSize=1;
+        mfirePower=1;
         this.mTypeShip="submarine";
-    }
-    
-    
+    }  
 }
-
